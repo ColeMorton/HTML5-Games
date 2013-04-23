ad62cfe5b0b2c887687285d50dc6480aa64de6f1
ColeMorton <Cole.Morton@charmhealth.com.au>
2013-04-23 12:07:56 +1000

    Added load resource and animation code.

--- Froot-Wars/js/game.js
+++ Froot-Wars/js/game.js
@@ -1,4 +1,30 @@
-$(window).load(function() {
+// Setup requestAnimationFrame and cancelAnimationFrame for use in the game code
+(function () {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+        window.cancelAnimationFrame =
+          window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function (callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function () { callback(currTime + timeToCall); },
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function (id) {
+            clearTimeout(id);
+        };
+}());
+
+$(window).load(function () {
     game.init();
 });
 
@@ -7,6 +33,7 @@ var game = {
     init:function(){
         // Initialise objects
         levels.init();
+        loader.init();
 
         // Hide all game layers and display the start screen
         $('.gamelayer').hide();
@@ -20,6 +47,48 @@ var game = {
     showLevelScreen: function () {
         $('.gamelayer').hide();
         $('#levelselectscreen').show('slow');
+    },
+    
+    // Game mode
+    mode: "intro",
+    
+    // X & Y Coordinates of the slingshot
+    slingshotX: 140,
+    slingshotY: 280,
+    start:function() {
+        $('.gamelayer').hide();
+        // Display the game canvas and score
+        $('#gamecanvas').show();
+        $('#scorescreen').show();
+
+        game.mode = "intro";
+        game.offsetLeft = 0;
+        game.ended = false;
+        game.cancelAnimationFrame = window.requestAnimationFrame(game.animate, game.canvas);
+    },
+    
+    handlePanning:function() {
+        game.offsetLeft++; // Temporary placeholder - keep panning to the right
+    },
+    
+    animate:function() {
+        // Animate the background
+        game.handlePanning();
+        
+        // Animate the characters
+        
+        // Draw the background with parallax scrolling
+
+        game.context.drawImage(game.currentLevel.backgroundImage, game.offsetLeft / 4, 0, 640, 480, 0, 0, 640, 480);
+        game.context.drawImage(game.currentLevel.foregroundImage, game.offsetLeft, 0, 640, 480, 0, 0, 640, 480);
+        
+        // Draw the slingshot
+        game.context.drawImage(game.slingshotImage, game.slingshotX - game.offsetLeft, game.slingshotY);
+        game.context.drawImage(game.slingshotFrontImage, game.slingshotX - game.offsetLeft, game.slingshotY);
+        
+        if (!game.ended) {
+            game.cancelAnimationFrame = window.requestAnimationFrame(game.animate, game.canvas);
+        }
     },
 };
 
@@ -40,25 +109,41 @@ var levels = {
         }
     ],
 
-    // Initialise level selection screen
-    init: function() {
+    // Initialize level selection screen
+    init:function(){
         var html = "";
         for (var i = 0; i < levels.data.length; i++) {
             var level = levels.data[i];
-            html += '<input type="button" value="' + (i + 1) + '">';
-        }
-
+            html += '<input type="button" value="' + (i + 1) + '"/>';
+        };
         $('#levelselectscreen').html(html);
-
         // Set the button click event handlers to load level
-        $('#levelselectscreen input').click(function() {
-            levels.load(this.value - 1);
+        $('#levelselectscreen input').click(function(){
+            levels.load(this.value-1);
             $('#levelselectscreen').hide();
         });
     },
 
     // Load all data and images for a specific level
-    load: function(number) {
+    load: function (number) {
+        //declare a new currentLevel object
+        game.currentLevel = { number: number, hero: [] };
+        game.score = 0;
+        $('#score').html('Score: ' + game.score);
+        var level = levels.data[number];
+
+        // Load the background, foreground and slingshot images
+        game.currentLevel.backgroundImage = loader.loadImage("images/backgrounds/" + level.background + ".png");
+        game.currentLevel.backgroundImage = loader.loadImage("images/backgrounds/" + level.foreground + ".png");
+        game.slingshotImage = loader.loadImage("images/slingshot.png");
+        game.slingshotFrontImage = loader.loadImage("images/slingshot-front.png");
+        
+        // Call game.start() once the assets have loaded
+        if (loader.loaded) {
+            game.start();
+        } else {
+            loader.onload = game.start;
+        }
     }
 };
 
@@ -89,13 +174,37 @@ var loader = {
         this.totalCount++;
         this.loaded = false;
         $('#loadingscreen').show();
-        var image = new image();
+        var image = new Image();
         image.src = url;
         image.onload = loader.itemLoaded;
         return image;
     },
     
+    soundFileExtn: ".ogg",
+    
+    loadSound:function() {
+        this.totalCount++;
+        this.loaded = false;
+        $('#loadingscreen').show();
+        var audio = new Audio();
+        audio.src = url + loader.soundFileExtn;
+        audio.addEventListener("canplaythough", loader.itemloaded, false);
+        return audio;
+    },
+    
     itemLoaded:function() {
         loader.loadedCount++;
+        $('#loadingmessage').html('Loaded ' + loader.loadedCount + ' of ' + loader.totalCount);
+        if (loader.loadedCount === loader.totalCount) {
+            // Loader has loaded completely..
+            loader.loaded = true;
+            // Hide the loading screen
+            $('#loadingscreen').hide();
+            // and call the loader.onload method if it exists
+            if (loader.onload) {
+                loader.onload();
+                loader.onload = undefined;
+            }
+        }
     }
 };
